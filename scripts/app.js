15022fc53a56dda18517641c240980a28e52b179
antoniodelhierro <delhierroantonio@icloud.com>
2022-06-03 01:10:16 -0500

    termine, ya estoy todo loco

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,6 @@
 const cards = document.getElementById('cards');
+const items = document.getElementById('items');
+const footer = document.getElementById('footer');
 const templateCard = document.getElementById('template-card').content;
 const templateCarrito = document.getElementById('template-carrito').content;
 const templateFooter = document.getElementById('template-footer').content;
@@ -6,27 +8,34 @@ const fragment = document.createDocumentFragment();
 
 let carrito = {};
 
-// SIMULE EL CONSUMIR UNA BASE DE DATOS USANDO EL JSON
-// leer la API cuando el html esta completamente cargado
+// 01- SIMULAR CONSUMIR UNA BASE DE DATOS USANDO EL JSON
 document.addEventListener('DOMContentLoaded', () => {
     fetchData();
+    if (localStorage.getItem('carrito')) {
+        carrito = JSON.parse(localStorage.getItem('carrito'));
+        mostrarCarrito();
+    }
 })
 
 cards.addEventListener('click', e => {
     addCarrito(e);
 })
 
+items.addEventListener('click', e => {
+    btnMasMenos(e);
+})
+
 const fetchData = async () => {
     try {
         const res = await fetch('/scripts/api.json');
         const data = await res.json();
-        // console.log(data);
         pintarCards(data);
     } catch (error) {
         console.log('error al encontrar el archivo JSON');
     }
 }
 
+// 02- MOSTRAR EL HTML DE FORMA DINAMICA
 const pintarCards = data => {
     data.forEach(producto => {
         templateCard.querySelector('h5').textContent = producto.titulo;
@@ -40,7 +49,7 @@ const pintarCards = data => {
     // pasarlo al fragment
     cards.appendChild(fragment);
 }
-
+// 03- AGREGAR LOS ITEMS AL CARRITO
 const addCarrito = (e) => {
     if (e.target.classList.contains('btn-dark')) {
         setCarrito(e.target.parentElement);
@@ -49,6 +58,7 @@ const addCarrito = (e) => {
     e.stopPropagation();
 }
 
+// 04- SUMAMOS LOS ITEMS DEL CARRITO Y MOSTRAMOS
 const setCarrito = objeto => {
     const producto = {
         id: objeto.querySelector('.btn-dark').dataset.id,
@@ -64,5 +74,81 @@ const setCarrito = objeto => {
     carrito[producto.id] = {
         ...producto
     }
-    console.log(carrito);
+    mostrarCarrito();
 }
+
+const mostrarCarrito = () => {
+    items.innerHTML = '';
+    Object.values(carrito).forEach(producto => {
+        templateCarrito.querySelector('th').textContent = producto.id;
+        templateCarrito.querySelectorAll('td')[0].textContent = producto.titulo;
+        templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad;
+        templateCarrito.querySelector('.btn-info').dataset.id = producto.id;
+        templateCarrito.querySelector('.btn-danger').dataset.id = producto.id;
+        templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio;
+        // clonar el template igual que el anterior
+        const clone = templateCarrito.cloneNode(true);
+        fragment.appendChild(clone);
+    })
+    items.appendChild(fragment);
+    // limpiamos el contenido del footer
+    mostrarFooter();
+    // guardamos carrito en local storage
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+// 05- MOSTRAMOS LOS DATOS EN EL FOOTER CON EL RECUENTO
+const mostrarFooter = () => {
+    footer.innerHTML = '';
+    if (Object.keys(carrito).length === 0) {
+        footer.innerHTML = `<th scope="row" colspan="5">Su carrito esta vacio, empiece a hacer sus compras!</th>`;
+        // hacer que no se sigan haciendo las operaciones
+        return;
+    }
+    // calculamos la cantidad de productos
+    const nCantidad = Object.values(carrito).reduce((acc, {
+        cantidad
+    }) => acc + cantidad, 0)
+    // calculamos los precios de los productos
+    const nPrecio = Object.values(carrito).reduce((acc, {
+        cantidad,
+        precio
+    }) => acc + cantidad * precio, 0)
+    templateFooter.querySelectorAll('td')[0].textContent = nCantidad;
+    templateFooter.querySelector('span').textContent = nPrecio;
+
+    const clone = templateFooter.cloneNode(true);
+    fragment.appendChild(clone);
+    footer.appendChild(fragment);
+
+    const vaciarBtn = document.getElementById('vaciar-carrito');
+    vaciarBtn.addEventListener('click', () => {
+        carrito = {};
+        mostrarCarrito();
+    })
+}
+
+// 06- ELIMINAMOS ITEMS DEL CARRITO
+const btnMasMenos = e => {
+    // aumenta la cantidad del producto
+    if (e.target.classList.contains('btn-info')) {
+        const producto = carrito[e.target.dataset.id];
+        producto.cantidad++;
+        carrito[e.target.dataset.id] = {
+            ...producto
+        };
+        mostrarCarrito();
+    }
+    // disminuye la cantidad del producto
+    if (e.target.classList.contains('btn-danger')) {
+        const producto = carrito[e.target.dataset.id];
+        producto.cantidad--;
+        if (producto.cantidad === 0) {
+            delete carrito[e.target.dataset.id];
+        }
+        mostrarCarrito();
+    }
+    e.stopPropagation();
+}
+
+// 07- GUARDAMOS EN LOCAL STORGE
